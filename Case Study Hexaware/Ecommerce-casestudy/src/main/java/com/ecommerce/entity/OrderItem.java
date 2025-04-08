26704fe8758f642ac8826f8bcb8eda8d07f5a93c
Monish Coumar <116776241+mc-monish28@users.noreply.github.com>
2025-04-08 10:29:16 +0530

    Add files via upload

---
+++ Case Study Hexaware/Ecommerce-casestudy/src/main/java/com/ecommerce/entity/OrderItem.java
@@ -0,0 +1,63 @@
+package com.ecommerce.entity;
+
+public class OrderItem {
+    private int orderItemId;
+    private int orderId;
+    private int productId;
+    private int quantity;
+
+    // Default constructor
+    public OrderItem() {
+    }
+
+    // Parameterized constructor
+    public OrderItem(int orderItemId, int orderId, int productId, int quantity) {
+        this.orderItemId = orderItemId;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    // Getters and Setters
+    public int getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(int orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderItem{" +
+                "orderItemId=" + orderItemId +
+                ", orderId=" + orderId +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
