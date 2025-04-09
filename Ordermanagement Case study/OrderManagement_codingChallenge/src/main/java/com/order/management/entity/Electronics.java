5f75279d795b9f6520e35260b967d48610a93204
Monish Coumar <116776241+mc-monish28@users.noreply.github.com>
2025-04-09 12:29:04 +0530

    Add files via upload

---
+++ Ordermanagement Case study/OrderManagement_codingChallenge/src/main/java/com/order/management/entity/Electronics.java
@@ -0,0 +1,34 @@
+package com.order.management.entity;
+
+public class Electronics extends Product {
+    private String brand;
+    private int warrantyPeriod;
+
+    public Electronics() {
+        super();
+    }
+
+    public Electronics(int productId, String productName, String description, double price, 
+                      int quantityInStock, String brand, int warrantyPeriod) {
+        super(productId, productName, description, price, quantityInStock, "Electronics");
+        this.brand = brand;
+        this.warrantyPeriod = warrantyPeriod;
+    }
+
+    // Getters and Setters
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getWarrantyPeriod() {
+        return warrantyPeriod;
+    }
+
+    public void setWarrantyPeriod(int warrantyPeriod) {
+        this.warrantyPeriod = warrantyPeriod;
+    }
+} 
---
+++ Ordermanagement Case study/OrderManagement_codingChallenge/src/main/java/com/order/management/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.order.management.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+} 
