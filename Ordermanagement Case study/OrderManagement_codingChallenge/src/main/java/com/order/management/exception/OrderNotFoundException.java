5d69b866ffc97dc20f7ea79d3d3f229fa2b0fc30
Monish Coumar <116776241+mc-monish28@users.noreply.github.com>
2025-04-09 12:29:42 +0530

    Delete Ordermanagement Case study directory

--- Ordermanagement Case study/OrderManagement_codingChallenge/src/main/java/com/order/management/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.order.management.exception;
-
-public class OrderNotFoundException extends Exception {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-} 
